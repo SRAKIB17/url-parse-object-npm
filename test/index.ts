0511332801de45d2facbb0eb7396f2d22f98fc24
SRAKIB17 <rakibulssc5@gmail.com>
2024-04-30 17:23:59 +0600

    'done

--- test/index.ts
+++ test/index.ts
@@ -36,7 +36,7 @@ export default
 
     const queryRegex = /\?([^#]*)/,
       authRegex = /\/\/(?:([^:]+)(?::([^@]+)))?/,
-      pathnameRegex = /(?:^[^:]+:\/\/[^/]+)?(\/[^?#]*)/,
+      pathnameRegex = /(?:^[^:]+:\/\/[^/]+)?(\/[^?#\/]*)/,
       portRegex = /:(\d+)/,
       hashRegex = /#([^]*)/,
       protocolRegex = /^(?:([^:]+):\/\/)?(?:([^:]+))/,
